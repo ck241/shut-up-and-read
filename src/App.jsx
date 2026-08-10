db5a95332ebfc8b31c86e1b880cd16309b6dd354
ck241 <295834703+ck241@users.noreply.github.com>
2026-08-10 14:19:54 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { Link, Route, Routes } from 'react-router-dom'
+
+function HomePage() {
+  return <h1>Hauptseite</h1>
+}
+
+function EventPage() {
+  return <h1>Event</h1>
+}
+
+function LoginPage() {
+  return <h1>Login</h1>
+}
+
+function SignupPage() {
+  return <h1>Registrieren</h1>
+}
+
+function NotFoundPage() {
+  return <h1>404 – Seite nicht gefunden</h1>
+}
+
+function App() {
+  return (
+    <>
+      <nav aria-label="Hauptnavigation">
+        <Link to="/">Startseite</Link>{' '}
+        <Link to="/events/1">Event</Link>{' '}
+        <Link to="/login">Login</Link>{' '}
+        <Link to="/signup">Registrieren</Link>
+      </nav>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/events/:id" element={<EventPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </>
+  )
+}
+
+export default App
