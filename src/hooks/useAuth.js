65bec2b0aa1493e01a7e4e4fea3aeb04bdce870b
philipp lang <philipp@aweos.de>
2026-08-11 16:37:44 +0200

    Add signup page

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,10 +3,14 @@ import {useNavigate} from 'react-router-dom';
 export default function useAuth() {
   const navigate = useNavigate();
 
-  async function login(email, password) {
+  function validate(email, password) {
     if (!email || !password) {
       throw new Error('Bitte fülle sowohl E-Mail-Adresse als auch Passwort aus.');
     }
+  }
+
+  async function login(email, password) {
+    validate(email, password);
 
     const response = await fetch('http://localhost:3001/api/auth/login', {
       method: 'POST',
@@ -25,7 +29,27 @@ export default function useAuth() {
     navigate('/');
   }
 
+  async function register(email, password) {
+    validate(email, password);
+
+    const response = await fetch('http://localhost:3001/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email, password}),
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || 'Registrierung fehlgeschlagen. Bitte überprüfe deine Angaben.');
+    }
+
+    navigate('/auth/login');
+  }
+
   return {
     login,
+    register,
   };
 }
