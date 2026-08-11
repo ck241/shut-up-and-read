d609115670eab0ffad09d9b91fc7a06ccaaa8d89
philipp lang <philipp@aweos.de>
2026-08-11 15:36:36 +0200

    add login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,50 @@
 import {Link} from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import FullLayout from '../layouts/FullLayout';
+import {useState} from 'react';
 
 function LoginPage() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  function setFormValue(key, value) {
+    setForm((f) => {
+      return {
+        ...f,
+        [key]: value,
+      };
+    });
+  }
+
+  async function onLogin(e) {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:3001/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    });
+
+    console.log(await response.json());
+    // Handle login logic here
+  }
+
   return (
     <FullLayout header="Anmelden und weiterlesen" subheader="Willkommen zurück">
-      <div className="w-full rounded-4xl border border-red-950/10 bg-white/90 p-8 shadow-2xl shadow-cyan-950/10 backdrop-blur-sm sm:p-10">
+      <form onSubmit={onLogin} className="w-full rounded-4xl border border-red-950/10 bg-white/90 p-8 shadow-2xl shadow-cyan-950/10 backdrop-blur-sm sm:p-10">
         <img src={logo} alt="Logo" className="mx-auto h-28 w-auto" />
         <div className="grid gap-6">
           <label className="grid gap-2 text-sm font-semibold text-red-950">
             E-Mail-Adresse
             <input
+              autoComplete="username"
               type="email"
+              value={form.email}
+              onInput={(e) => setFormValue('email', e.target.value)}
               placeholder="name@example.com"
               className="h-14 w-full rounded-2xl border border-stone-200 bg-stone-50 px-4 text-base text-red-950 outline-none transition focus:border-cyan-900 focus:ring-4 focus:ring-cyan-900/10"
             />
@@ -20,9 +53,13 @@ function LoginPage() {
           <label className="grid gap-2 text-sm font-semibold text-red-950">
             Passwort
             <input
+              autoComplete="current-password"
+              minLength="8"
               type="password"
               placeholder="••••••••"
               className="h-14 w-full rounded-2xl border border-stone-200 bg-stone-50 px-4 text-base text-red-950 outline-none transition focus:border-cyan-900 focus:ring-4 focus:ring-cyan-900/10"
+              value={form.password}
+              onInput={(e) => setFormValue('password', e.target.value)}
             />
           </label>
 
@@ -37,7 +74,7 @@ function LoginPage() {
             </Link>
           </p>
         </div>
-      </div>
+      </form>
     </FullLayout>
   );
 }
